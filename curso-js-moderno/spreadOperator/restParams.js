e9f9a22a856e172befed077b544fd4319fe427f1
viniaxt <viniciusaxt@gmail.com>
2019-05-30 16:51:09 -0300

    Added API ft. Roberto

--- curso-js-moderno/spreadOperator/restParams.js
+++ curso-js-moderno/spreadOperator/restParams.js
@@ -1,6 +1,10 @@
-function multiply (mult, ...args) {
-  return args.map(arg => mult * arg)
+function multiply (...args) {
+  const params = args.map(arg => arg)
+  console.log(params)
+  const { params } = params
+  console.log(a)
 }
 
 // rest params can receive multiple params, and they are not definde by you!
 console.log(multiply(2, 1, 2, 3, 4, 5, 6))
+multiply({a: 1, b: 2})
